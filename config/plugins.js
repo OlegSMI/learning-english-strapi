a124a5f244325285df7a07f802797661249a35c8
Alex Boytsov <slovplan@mail.ru>
2024-06-09 00:20:14 +0300

    edit fir deploying

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,12 @@
 module.exports = () => ({
   "vercel-deploy": {
     enabled: true,
+    config: {
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: "learning-english-strapi.vercel.app",
+      //   teamFilter: "your-team-id-on-vercel",
+      //   roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
   },
 });
